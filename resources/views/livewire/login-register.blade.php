74385789442ec3ec24e817ad378432490924baef
Philipp Lorenz <p.github@lorenz-ho.me>
2022-01-28 19:09:53 +0100

    Added missing cursor classes to login form

--- resources/views/livewire/login-register.blade.php
+++ resources/views/livewire/login-register.blade.php
@@ -49,8 +49,8 @@
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <input id="remember-me" name="remember-me" type="checkbox" wire:model="remember_me"
-                        class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-                    <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                        class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded cursor-pointer" />
+                    <label for="remember-me" class="ml-2 block text-sm text-gray-900 cursor-pointer">
                         Angemeldet bleiben
                     </label>
                 </div>
