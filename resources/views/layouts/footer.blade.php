3cc59f68ffd48fec17f74dec54c36342299259e9
Philipp Lorenz <p.github@lorenz-ho.me>
2022-11-11 20:56:11 +0100

    Update to Laravel 9

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,11 @@
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 h-full flex items-center">
             <div class="block px-2 sm:px-0 sm:flex w-full justify-between">
                 <div class="text-white font-semibold">LAN-Tool v1.0</div>
+				@if (date('Y') == '2022')
                 <div class="text-gray-400">&copy; {{ date('Y') }} Philipp &quot;D.Cent&quot; Lorenz</sdivpan>
+				@else
+                <div class="text-gray-400">&copy; 2022-{{ date('Y') }} Philipp &quot;D.Cent&quot; Lorenz</sdivpan>
+				@endif
             </div>
         </div>
     </div>
