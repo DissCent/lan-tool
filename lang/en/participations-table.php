3223b9d4971b88921dc417c322af4009ffc57384
Philipp Lorenz <p.github@lorenz-ho.me>
2024-03-12 20:58:16 +0100

    Fix missing translation

--- lang/en/participations-table.php
+++ lang/en/participations-table.php
@@ -12,7 +12,7 @@ return [
     'hidden' => 'hidden',
     'train-need-ride' => 'Train (pick-up)',
     'train-no-ride' => 'Train',
-    'car-space' => 'Auto (ride offer)',
+    'car-space' => 'Car (ride offer)',
     'car-no-space' => 'Car (full)',
     'joining-other' => 'Carpool',
     'unknown' => 'Unknown',
